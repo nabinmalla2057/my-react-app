2eada979af0cad0539ef8db8727bb96f6c7cbac3
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-15 09:45:01 +0545

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+
+  const countries = [
+    {
+      name: "USA",
+      cities: ["New York", "Los Angeles", "Chicago", "San Francisco"],
+    },
+    {
+      name: "India",
+      cities: ["Delhi", "Banglore", "Kolkata", "Kerala"],
+    },
+    {
+      name: "Nepal",
+      cities: ["Pokhara", "Kathmandu", "Dhangadhi", "Nepalgunj"],
+    },
+  ];
+
+  const handleCountryChange = (event) => {
+    const selectedCountry = event.target.value;
+
+    setSelectedCountry(selectedCountry);
+
+    setSelectedCity("");
+  };
+
+  const handleCityChange = (event) => {
+    const selectedCity = event.target.value;
+
+    setSelectedCity(selectedCity);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Country and City Dropdowns</h1>
+
+      <div className="select-container">
+        <label>Select a Country:</label>
+        <select value={selectedCountry} onChange={handleCountryChange}>
+          <option value="">Select a country</option>
+          {countries.map((country, index) => (
+            <option key={index} value={country.name}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {selectedCountry && (
+        <div className="select-container">
+          <label>Select a City:</label>
+          <select value={selectedCity} onChange={handleCityChange}>
+            <option value="">Select a city</option>
+            {countries
+              .find((country) => country.name === selectedCountry)
+              .cities.map((city, index) => (
+                <option key={index} value={city}>
+                  {city}
+                </option>
+              ))}
+          </select>
+        </div>
+      )}
+
+      {selectedCity && (
+        <div>
+          <p>
+            You selected: {selectedCountry}, {selectedCity}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
